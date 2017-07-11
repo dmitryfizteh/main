ca8edd24a04f4ee02c404886c13f8e6b4daf84b3
Dmitry Morozov <dmitryfizteh@gmail.com>
2017-07-11 22:05:37 +0300

    load_project.py: + добавлено наполнение таблицы projects

--- storefront/load_project.py
+++ storefront/load_project.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#TODO: проверка, что числовые поля не могут быть пустыми
+#TODO: в файле загрузке поправить все цифры привести к "руб"
+
 import xlrd
 import postgresql
 
@@ -13,7 +16,7 @@ f.close()
 # UNDO-скрипт
 undo =''
 
-dic = {"id":1}
+dic = {"":None}
 rb = xlrd.open_workbook('project1.xlsx')
 sheet = rb.sheet_by_index(0)
 for rownum in range(2, sheet.nrows):
@@ -26,91 +29,7 @@ for rownum in range(2, sheet.nrows):
 #    print("%s: %s" % (key, dic[key]))
 #print (dic)
 
-id = dic['id'] # id проекта
-name = dic['Наименование Проекта']
-prerequisite = dic['Предпосылки реализации Проекта']
-substance = dic['Суть Проекта']
-technologies = dic['Используемые в Проекте технологии']
-irrmonths = dic['IRR, %'] *100
-npv = dic['NPV']
-npvdiscount = dic['Ставка дисконтирования для расчета NPV, %']
-payoffperiodmonths = dic['Срок окупаемости полных инвестиционных затрат (дисконтированный)']
-payoffperiodmonthssimple = dic['Срок окупаемости полных инвестиционных затрат (простой)']
-firsttranche = dic['Планируемая дата получения первого транша (дата)']
-projectstatus = dic['Текущий статус проекта']
-investmentphaseperiodmonths = dic['Продолжительность инвестиционной фазы (мес)']
-loanperiodmonths = dic['Срок кредита (мес)']
-projectpowerperiodmonths = dic['Срок выхода на проектную мощность']
-cur_market_share_fed = dic['Текущий объем федерального рынка в денежном выражении']
-cur_market_share_reg = dic['Текущий объем регионального рынка в денежном выражении']
-cur_market_volume_fed = dic['Текущий объем федерального рынка в натуральном выражении']
-cur_market_volume_reg = dic['Текущий объем регионального рынка в натуральном выражении']
-plan_market_share_fed = dic['Планируемая доля инициаторов на федеральном рынке']
-plan_market_share_reg = dic['Планируемая доля инициаторов на региональном рынке']
-production_volume = dic['Объём производства в рамках создаваемого производства']
-dscr = dic['Средний коэффициент покрытия долга по проекту (DSCR)']
-loanrate = dic['Ожидаемая процентная ставка']
-requiredlicenses = dic['Необходимость получения лицензий в рамках Проекта']
-competitivefactors = dic['Факторы, обеспечивающие достижение планируемых показателей Проекта']
-competitiveness = dic['Конкурентные преимущества создаваемого производства и продукции']
-
-dic[''] = None
-
-requiredlicensesdescription = dic['']
-
-financeplanperiodmonths = dic['']
-
-stage_commodities_study = dic['']
-stage_dev_building_info = dic['Наличие строительной сметы и договора строительного подряда']
-stage_dev_licenses = dic['']
-stage_equipment_supply = dic['']
-stage_investments_volume_own = dic['']
-stage_sale_study = dic['']
-stage_investments_volume_total = dic['']
-
-organization_id = dic['']
-development_permission_id = dic['']
-land_right_type_id = dic['']
-project_examination_id = dic['']
-tech_condition_id = dic['']
-industry_id = dic['']
-project_type_id = dic['']
-region_id = dic['']
-
-str = "name = {}\n prerequisite = {}\n requiredlicenses = {}\n requiredlicensesdescription = {}\n substance = {}\n technologies = {}\n dscr = {}\n financeplanperiodmonths = {}\n irrmonths = {}\n" \
-      "npv = {}\n npvdiscount = {}\n payoffperiodmonths = {}\n payoffperiodmonthssimple = {}\n firsttranche = {}\n investmentphaseperiodmonths = {}\n loanperiodmonths = {}\n " \
-      "loanrate = {}\n projectpowerperiodmonths = {}\n competitivefactors = {}\n competitiveness = {}\n organization_id = {}\n projectstatus = {}\n stage_commodities_study = {}\n stage_dev_building_info = {}\n " \
-      "stage_dev_licenses = {}\n stage_equipment_supply = {}\n stage_investments_volume_own = {}\n stage_sale_study = {}\n stage_investments_volume_total = {}\n cur_market_share_fed = {}\n" \
-      "cur_market_share_reg = {}\n cur_market_volume_fed = {}\n cur_market_volume_reg = {}\n plan_market_share_fed = {}\n plan_market_share_reg = {}\n production_volume = {}\n " \
-      "development_permission_id = {}\n land_right_type_id = {}\n project_examination_id = {}\n tech_condition_id = {}\n industry_id = {}\n project_type_id = {}\n region_id = {}\n) " \
-      "".format(
-    name, prerequisite, requiredlicenses, requiredlicensesdescription, substance, technologies, dscr, financeplanperiodmonths, irrmonths,
-    npv, npvdiscount, payoffperiodmonths, payoffperiodmonthssimple, firsttranche, investmentphaseperiodmonths, loanperiodmonths,
-    loanrate, projectpowerperiodmonths, competitivefactors, competitiveness, organization_id, projectstatus, stage_commodities_study, stage_dev_building_info,
-    stage_dev_licenses, stage_equipment_supply, stage_investments_volume_own, stage_sale_study, stage_investments_volume_total, cur_market_share_fed,
-    cur_market_share_reg, cur_market_volume_fed, cur_market_volume_reg, plan_market_share_fed, plan_market_share_reg, production_volume,
-    development_permission_id, land_right_type_id, project_examination_id, tech_condition_id, industry_id, project_type_id, region_id)
-#print(str)
-
-# TODO: создаем проект
-str = "INSERT INTO public.projects(id, name, prerequisite, requiredlicenses, requiredlicensesdescription, substance, technologies, dscr, financeplanperiodmonths, irrmonths," \
-      "npv, npvdiscount, payoffperiodmonths, payoffperiodmonthssimple, firsttranche, investmentphaseperiodmonths, loanperiodmonths, " \
-      "loanrate, projectpowerperiodmonths, competitivefactors, competitiveness, organization_id, projectstatus, stage_commodities_study, stage_dev_building_info, " \
-      "stage_dev_licenses, stage_equipment_supply, stage_investments_volume_own, stage_sale_study, stage_investments_volume_total, cur_market_share_fed," \
-      "cur_market_share_reg, cur_market_volume_fed, cur_market_volume_reg, plan_market_share_fed, plan_market_share_reg, production_volume, " \
-      "development_permission_id, land_right_type_id, project_examination_id, tech_condition_id, industry_id, project_type_id, region_id) " \
-      "VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'" \
-      ", '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(
-    id, name, prerequisite, requiredlicenses, requiredlicensesdescription, substance, technologies, dscr, financeplanperiodmonths, irrmonths,
-    npv, npvdiscount, payoffperiodmonths, payoffperiodmonthssimple, firsttranche, investmentphaseperiodmonths, loanperiodmonths,
-    loanrate, projectpowerperiodmonths, competitivefactors, competitiveness, organization_id, projectstatus, stage_commodities_study, stage_dev_building_info,
-    stage_dev_licenses, stage_equipment_supply, stage_investments_volume_own, stage_sale_study, stage_investments_volume_total, cur_market_share_fed,
-    cur_market_share_reg, cur_market_volume_fed, cur_market_volume_reg, plan_market_share_fed, plan_market_share_reg, production_volume,
-    development_permission_id, land_right_type_id, project_examination_id, tech_condition_id, industry_id, project_type_id, region_id)
-
-print(str)
-
-#TODO: заменить id на запрос max id
+# Заполнение информации о финансовой нагрузке инициатора
 id_sql = db.query("SELECT MAX(id) FROM public.organization_finances;");
 if (id_sql[0][0] is None):
     id_org_fin = 1
@@ -128,7 +47,7 @@ str = "INSERT INTO public.organization_finances(" \
       "fininfo_prof_np, fininfo_requested_credit, fininfo_revenue, fininfo_revenue_4q, " \
       "fininfo_sales_profit, fininfo_credits_short, fininfo_loans_short, " \
       "fininfo_investments_shortterm, fininfo_subsidies, fininfo_tot_liquidity_ratio, " \
-      "fininfo_working_capital)" \
+      "fininfo_working_capital) " \
       "VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', " \
       "'{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}','{}', '{}', '{}','{}', '{}', '{}', '{}');".format(
     id_org_fin, dic['Кредиторская задолженность'], dic['Валюта Баланса'], dic['Прибыль до налогообложения'],
@@ -143,14 +62,90 @@ str = "INSERT INTO public.organization_finances(" \
     dic['Краткосрочные финансовые вложения'], dic['Субсидии полученные, за период'], dic['Коэффициент общей ликвидности'],
     dic['Собственные оборотные средства'])
 print(str)
-
 undo = "DELETE FROM public.organization_finances WHERE id={0};\n".format(id_org_fin) + undo
 
-# TODO: создаем организацию
+
+# создаем организацию
+# TODO: проверить что такой организации еще нет в БД (и сделать это до внесения ее финданных)
+id_sql = db.query("SELECT MAX(id) FROM public.organizations;");
+if (id_sql[0][0] is None):
+    id_org = 1
+else:
+    id_org = int (id_sql[0][0]) + 1
+
+# TODO: уточнить про owner_grp (пока 1 - тестовые организации-создатели проектов)
+str = "INSERT INTO public.organizations(" \
+      "id, name, address, fullname, inn, kpp, ogrn, owner_grp_id, loaninfo_ebitda," \
+      "loaninfo_guarantee_to_3rd, loaninfo_interest_pmts, loaninfo_interest_pmts_prj," \
+      "loaninfo_total_credits, loaninfo_total_credits_with_prj, loaninfo_orders_to_3rd," \
+      "fininfo_id) " \
+      "VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(
+    id_org, dic['Инициатор проекта'], dic['Почтовый адрес инициатора'], dic['Полное название инициатора'],dic['ИНН'],dic['КПП'],dic['ОГРН'], 1, dic['EBITDA'],
+    dic['Объем выданных поручительств Заемщиком в пользу третьих лиц'],dic['Процентные платежи'],dic['Процентные платежи с учётом проекта'],
+    dic['Общий долг'],dic['Общий долг с учётом проекта'],dic['Объём выданных поручений третьим лицам'], id_org_fin)
+print(str)
+undo = "DELETE FROM public.organizations WHERE id={0};\n".format(id_org) + undo
+
+# TODO: создаем проект
+
+id_sql = db.query("SELECT MAX(id) FROM public.projects;");
+if (id_sql[0][0] is None):
+    id_proj= 1
+else:
+    id_proj = int (id_sql[0][0]) + 1
+
+#TODO: внести реальные id
+development_permission_id = 1
+land_right_type_id = 1
+project_examination_id = 1
+tech_condition_id = 1
+industry_id = 1
+project_type_id = 1
+region_id = 77
+government_support_id = 1
+
+str = "INSERT INTO public.projects(" \
+      "id, name, prerequisite, requiredlicenses, requiredlicensesdescription, " \
+      "substance, technologies, dscr, financeplanperiodmonths, irrmonths, " \
+      "npv, npvdiscount, payoffperiodmonths, payoffperiodmonthssimple, " \
+      "firsttranche, investmentphaseperiodmonths, loanperiodmonths, " \
+      "loanrate, projectpowerperiodmonths, competitivefactors, competitiveness, " \
+      "organization_id, projectstatus, stage_commodities_study, stage_dev_building_info, " \
+      "stage_dev_licenses, stage_equipment_supply, stage_investments_volume_own, " \
+      "stage_sale_study, stage_investments_volume_total, cur_market_share_fed, " \
+      "cur_market_share_reg, cur_market_volume_fed, cur_market_volume_reg, " \
+      "plan_market_share_fed, plan_market_share_reg, production_volume, " \
+      "development_permission_id, land_right_type_id, project_examination_id, " \
+      "tech_condition_id, industry_id, project_type_id, region_id, project_total_cost, " \
+      "government_support_id) " \
+      "VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', " \
+      "'{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(
+            id_proj, dic['Наименование Проекта'], dic['Предпосылки реализации Проекта'], dic['Необходимость получения лицензий в рамках Проекта'],
+            dic['Необходимый набор лицензий'], dic['Суть Проекта'], dic['Используемые в Проекте технологии'],
+            dic['Средний коэффициент покрытия долга по проекту (DSCR)'], int(dic['Горизонт планирования, месяцев']), dic['IRR, %'],
+            dic['NPV'], dic['Ставка дисконтирования для расчета NPV, %'], int(dic['Срок окупаемости полных инвестиционных затрат (дисконтированный)']),
+            int(dic['Срок окупаемости полных инвестиционных затрат (простой)']),
+            dic['Планируемая дата получения первого транша (дата)'], int(dic['Продолжительность инвестиционной фазы (мес)']), int(dic['Срок кредита (мес)']),
+            dic['Ожидаемая процентная ставка, %'], int(dic['Срок выхода на проектную мощность']), dic['Факторы, обеспечивающие достижение планируемых показателей Проекта'],
+            dic['Конкурентные преимущества создаваемого производства и продукции'],
+            id_org, dic['Текущий статус проекта'], dic['Проработанность вопросов снабжения Проекта сырьем'], dic['Наличие строительной сметы и договора строительного подряда'],
+            dic['Наличие лицензий'], dic['Наличие оборудования'], dic['собственные средства инициатора Проекта'],
+            dic['Проработанность вопросов сбыта продукции Проекта'], dic['Объем вложенных средств в Проект'], dic['Текущая доля инициаторов на федеральном рынке Проекта'],
+            dic['Текущая доля инициаторов на региональном рынке Проекта'], dic['Текущий объем федерального рынка'], dic['Текущий объем регионального рынка'],
+            dic['Планируемая доля инициаторов на федеральном рынке'], dic['Планируемая доля инициаторов на региональном рынке'], dic['Объём производства в рамках создаваемого производства'],
+            development_permission_id, land_right_type_id, project_examination_id,
+            tech_condition_id, industry_id, project_type_id, region_id, dic['Общая стоимость Проекта'],
+            government_support_id)
+print(str)
+undo = "DELETE FROM public.projects WHERE id={0};\n".format(id_proj) + undo
+
 # TODO: связываем проект и организацию через project_initiators
+
+
+# TODO: добавить документы по проекту
 # TODO: создаем источники финансирования
 # TODO: создаем источники поддержки
 # TODO: создаем залоги
-# TODO: создаем стоимость проекта
+# TODO: создаем бюджет проекта
 # TODO: создаем проекты в тех же отраслях
 # TODO: создаем проекты в смежных отраслях
